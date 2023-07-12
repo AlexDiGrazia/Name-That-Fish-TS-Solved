4b1421b55a9b26317f419d4256bd1f37f7f2df40
Alex Digrazia <alexndigrazia@hotmail.com>
2023-07-11 19:23:08 -0700

    refactored functional app based on feedback from Andrei and Tyson

--- src/Components/Functional/FunctionalApp.tsx
+++ src/Components/Functional/FunctionalApp.tsx
@@ -5,21 +5,20 @@ import { initialFishes } from "../../constants";
 import { useState } from "react";
 
 export function FunctionalApp() {
-  const [index, setIndex] = useState(0);
   const [score, setScore] = useState({ correct: 0, inCorrect: 0 });
 
+  const index = score.correct + score.inCorrect;
   const currentFish = initialFishes[index];
   const answersLeft = initialFishes.map((obj) => obj.name).slice(index);
   const totalCount = initialFishes.length;
   const gameInProgress = index !== 4;
 
-  const handleAnswer = (fishName: string) => {
-    const valueToAdd = fishName === currentFish.name ? [1, 0] : [0, 1];
+  const handleAnswer = (userInput: string) => {
+    const valueToAdd = userInput === currentFish.name ? [1, 0] : [0, 1];
     setScore({
       correct: score.correct + valueToAdd[0],
       inCorrect: score.inCorrect + valueToAdd[1],
     });
-    setIndex(index + 1);
   };
 
   return (
